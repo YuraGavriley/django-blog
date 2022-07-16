01baf2ad081dadeef0fa7f5aaea34e14c81f6029
Yura <yura.gavriley@gmail.com>
2022-07-16 11:44:51 +0300

    second commit

--- my_new_site/settings.py
+++ my_new_site/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d))r2dm^xuy4jm-=+@6!o37z!t3&2bfd55+m%$43-we+q4hlne'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
